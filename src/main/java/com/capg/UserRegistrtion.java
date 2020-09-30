c91bf478d907162c220e0e790851e8a4731ee5da
mehakjit <mehakjitsingh@gmail.com>
2020-10-01 01:14:10 +0530

    [Mehakjit] Added: Welcome page.

---
+++ src/main/java/com/capg/UserRegistrtion.java
@@ -0,0 +1,11 @@
+package com.capg;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrtion {
+	public static void main(String[] args) {System.out.println("Welcome to User Registration Wizard");
+	}
+}
+	
